324b78f82706a9d88f057c54b4289fce5795c175
panditdhamdhere <dhamdherepandit@gmail.com>
2024-09-12 11:29:11 +0530

    deployment upadted

--- test/MerkleAirdropTest.t.sol
+++ test/MerkleAirdropTest.t.sol
@@ -4,23 +4,48 @@ pragma solidity ^0.8.24;
 import {Test, console} from "forge-std/Test.sol";
 import {MerkleAirdrop} from "../src/MerkleAirdrop.sol";
 import {MisalToken} from "../src/MisalToken.sol";
+import {ZkSyncChainChecker} from "lib/foundry-devops/src/ZkSyncChainChecker.sol";
+import {DeployMerkleAirdrop} from "../script/DeployMerkleAirdrop.s.sol";
 
-contract MerkleAirdropTest is Test {
+contract MerkleAirdropTest is ZkSyncChainChecker, Test {
     MerkleAirdrop public airdrop;
     MisalToken public token;
 
     bytes32 public ROOT =
         0xaa5d581231e596618465a56aa0f5870ba6e20785fe436d5bfb82b08662ccc7c4;
+    uint256 AMOUNT_TO_CLAIM = 25 * 1e18;
+    uint256 public AMOUNT_TO_SEND = AMOUNT_TO_CLAIM * 4;
+    bytes32 proofOne =
+        0x0fd7c981d39bece61f7499702bf59b3114a90e66b51ba2c53abdf7b62986c00a;
+    bytes32 proofTwo =
+        0xe5ebd1e1b5a5478a944ecab36a9a954ac3b6b8216875f6524caa7a1d87096576;
+    bytes32[] public PROOF = [proofOne, proofTwo];
+
     address user;
     uint256 userPrivKey;
 
     function setUp() public {
-        token = new MisalToken();
-        airdrop = new MerkleAirdrop(ROOT, token);
-        (user, userPrivKey) = makeAddrAndKey("user");
+        if (!isZkSyncChain()) {
+            // deploy with script
+            DeployMerkleAirdrop deployer = new DeployMerkleAirdrop();
+            (airdrop, token) = deployer.deployMerkleAirdrop();
+        } else {
+            token = new MisalToken();
+            airdrop = new MerkleAirdrop(ROOT, token);
+            token.mint(token.owner(), AMOUNT_TO_SEND);
+            token.transfer(address(airdrop), AMOUNT_TO_SEND);
+            (user, userPrivKey) = makeAddrAndKey("user");
+        }
     }
 
     function testUsersCanClaim() public {
-        console.log("user address:", user);
+        uint256 startingBalance = token.balanceOf(user);
+
+        vm.prank(user);
+        airdrop.claim(user, AMOUNT_TO_CLAIM, PROOF);
+
+        uint256 endingBalance = token.balanceOf(user);
+        console.log("airdrop address this tokens:", endingBalance);
+        assertEq(endingBalance - startingBalance, AMOUNT_TO_CLAIM);
     }
 }
